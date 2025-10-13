f62571ad0075ce06f4d2064541bfce89f963588e
Krishna <kp8353156@gmail.com>
2025-10-13 08:16:00 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+    return (
+        <header className="bg-black">
+            <nav className="flex items-center justify-between max-w-7xl mx-auto px-6 py-4">
+                <img src="/logo.svg" alt="Apple logo" />
+                <ul className="hidden md:flex items-center justify-between text-white gap-8" >
+                    {navLinks.map((link) => (
+                        <li key={link.label} >
+                            <a href={link.label} >{link.label}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="flex-center gap-3">
+                    <button className="cursor-pointer mx-2" >
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button className="cursor-pointer mx-2" >
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+export default Navbar;
